f486c8cfe49318dc81b859f0a792c280284a64ed
artizin9 <reis08897@gmail.com>
2025-06-16 13:07:33 -0300

    deployt

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,4 +1,8 @@
 import Redis from "ioredis";
 import { env } from "./env";
 
-export const redis = new Redis(env.REDIS_URL);
+export const redis = new Redis(env.REDIS_URL, {
+  tls: {},
+  maxRetriesPerRequest: null,
+  reconnectOnError: () => true,
+});
