9fa85efde9ea0ce738b15e37c55838263a581008
artizin9 <reis08897@gmail.com>
2025-08-19 14:45:20 -0300

    fix

--- src/domain/entities/news.ts
+++ src/domain/entities/news.ts
@@ -4,6 +4,7 @@ export class News {
         public title: string,
         public content: string,
         public adminId: string,
+        public author: string,
         public photo: string,
         public date: Date = new Date()
     ){}
--- src/infra/schemas/newsSchema.ts
+++ src/infra/schemas/newsSchema.ts
@@ -4,6 +4,7 @@ export const newsSchema = z.object({
   title: z.string().min(3, "Título muito curto"),
   content: z.string().min(10, "Conteúdo muito curto"),
   adminId: z.string().uuid(),
+  author: z.string(),
   photoURLs: z.string(),
   date: z.date().default(() => new Date())
 });
