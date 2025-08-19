c0d64afd4cde4cbb673f08f9622175dccb9694de
ChHenrique <ch1claudiohenrique@gmail.com>
2025-08-19 13:29:20 -0300

    feat: interface methods completed

--- src/domain/repositorys/INewsRepository.ts
+++ src/domain/repositorys/INewsRepository.ts
@@ -2,6 +2,9 @@ import { News } from "../entities/news";
 
 export interface INewsRepository {
     getAll(): Promise<News[]>;
-    getById(): Promise<News>
-    
+    getById(id: string): Promise<News | null>;
+    create(data: News): Promise<News | null>;
+    update(data: Partial<News>): Promise<News | null>;
+    delete(id: string): Promise<void>;
+
 }
