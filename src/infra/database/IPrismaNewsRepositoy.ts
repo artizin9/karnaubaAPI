f8d7b187e8f430efef3794f204ef467f31ea625b
artizin9 <reis08897@gmail.com>
2025-08-19 13:38:31 -0300

    feat: method create and update, finally

--- src/infra/database/IPrismaNewsRepositoy.ts
+++ src/infra/database/IPrismaNewsRepositoy.ts
@@ -1,3 +1,4 @@
+import { prisma } from "../../config/prisma";
 import { News } from "../../domain/entities/news";
 import { INewsRepository } from "../../domain/repositorys/INewsRepository";
 
@@ -15,10 +16,19 @@ export class IPrismaNewsRepository implements INewsRepository {
     }
 
     async create(data: News): Promise<News | null> {
-        
+        const news = await prisma.news.create({
+            data: {...data}
+        })
+
+        return news
     }
 
-    async update(data: Partial<News>): Promise<News | null> {
-        
+    async update(data: Partial<News>, id: string): Promise<News | null> {
+        const updatedNews = await prisma.news.update({
+            where: {id},
+            data: {...data}
+        })
+
+        return updatedNews
     }
 }
