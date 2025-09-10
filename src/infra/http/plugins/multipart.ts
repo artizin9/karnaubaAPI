f14e0bad7aefd1dc11280c42ab9ba850edfe1d5d
ChHenrique <ch1claudiohenrique@gmail.com>
2025-09-10 13:53:23 -0300

    novo

--- src/infra/http/plugins/multipart.ts
+++ src/infra/http/plugins/multipart.ts
@@ -4,55 +4,80 @@ import { PhotoStorageType } from "../../dto/photoStorageDTO";
 import { VideoStorageService } from "../../services/videoStorageService";
 import { Readable } from "stream";
 import ffmpeg from "fluent-ffmpeg";
+import ffmpegPath from "@ffmpeg-installer/ffmpeg";
+import ffprobePath from "@ffprobe-installer/ffprobe";
+
+// Configura os paths do ffmpeg/ffprobe
+ffmpeg.setFfmpegPath(ffmpegPath.path);
+ffmpeg.setFfprobePath(ffprobePath.path);
+
 
 export class Multipart {
-    constructor(private photoStorage: PhotoStorageService, private videoStorage: VideoStorageService) { };
+  constructor(
+    private photoStorage: PhotoStorageService,
+    private videoStorage: VideoStorageService
+  ) {}
 
-    async handleDataMultipart(req: FastifyRequest, type: PhotoStorageType, updatedPhoto: boolean = false) {
-        const data = req.parts();
-        let rawFields: any = {};
-        let photoURLs: string[] = [];
+  async handleDataMultipart(
+    req: FastifyRequest,
+    type: PhotoStorageType,
+    updatedPhoto: boolean = false
+  ) {
+    const data = req.parts();
+    let rawFields: any = {};
+    let photoURLs: string[] = [];
 
-        for await (const part of data) {
-            if (part.type === "file" && part.filename) {
-                const buffer = await part.toBuffer();
-                const { filename, mimetype } = part;
-                const url = await this.photoStorage.save({ buffer, filename, mimetype }, type);
-                photoURLs.push(url);
-            } else if (part.type === "field") {
-                rawFields[part.fieldname] = part.value;
-            }
-        }
-
-        rawFields.photoURLs = photoURLs;
-        return updatedPhoto ? rawFields.photoURL = photoURLs[0] : rawFields;
+    for await (const part of data) {
+      if (part.type === "file" && part.filename) {
+        const buffer = await part.toBuffer();
+        const { filename, mimetype } = part;
+        const url = await this.photoStorage.save(
+          { buffer, filename, mimetype },
+          type
+        );
+        photoURLs.push(url);
+      } else if (part.type === "field") {
+        rawFields[part.fieldname] = part.value;
+      }
     }
 
-    async handleVideoMultipart(req: FastifyRequest) {
-        const data = await req.parts()
-        let rawFields: any = {};
-        let fileName = null
-        for await (const part of data) {
-            if (part.type === 'file' && part.filename) {
-                const buffer = await part.toBuffer()
-                const { filename, mimetype } = part
+    rawFields.photoURLs = photoURLs;
+    return updatedPhoto ? (rawFields.photoURL = photoURLs[0]) : rawFields;
+  }
 
-                const url = await this.videoStorage.save({ buffer, filename, mimetype })
-                rawFields[part.fieldname] = url
-                fileName = filename
-            } else if (part.type === 'field') rawFields[part.fieldname] = part.value
-        }
+  async handleVideoMultipart(req: FastifyRequest) {
+    const data = await req.parts();
+    let rawFields: any = {};
+    let fileName = null;
+    
+    for await (const part of data) {
+      if (part.type === "file" && part.filename) {
+        const buffer = await part.toBuffer();
+        const { filename, mimetype } = part;
 
-        const stream = Readable.from(rawFields.buffer)
-        const duration = await new Promise<number>((resolve, reject) => {
-            ffmpeg(stream)
-                .inputFormat(rawFields.mimetype?.split("/")[1])
-                .ffprobe((err, metadata) => {
-                    if (err) return reject(err);
-                    resolve(metadata.format.duration || 0);
-                });
+        const url = await this.videoStorage.save({
+          buffer,
+          filename,
+          mimetype,
         });
+        rawFields[part.fieldname] = url;
 
-        return { rawFields, duration, fileName }
+        rawFields.buffer = buffer;
+        rawFields.mimetype = mimetype;
+        fileName = filename;
+      } else if (part.type === "field") rawFields[part.fieldname] = part.value;
     }
+
+    const stream = Readable.from(rawFields.buffer);
+    const duration = await new Promise<number>((resolve, reject) => {
+      ffmpeg(stream)
+        .inputFormat(rawFields.mimetype?.split("/")[1])
+        .ffprobe((err, metadata) => {
+          if (err) return reject(err);
+          resolve(metadata.format.duration || 0);
+        });
+    });
+
+    return { rawFields, duration, fileName };
+  }
 }
